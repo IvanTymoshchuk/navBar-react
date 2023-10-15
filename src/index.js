7176c1ac0add6e87e33d99ac1e9c82bdf93d124b
Ivan Tymoshchuk <timohin2231@gmail.com>
2023-10-15 13:24:07 +0200

    fix

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-   <BrowserRouter basename="/navBar">
+   <BrowserRouter basename="/navBar-react">
   <App />
 </BrowserRouter>
   </React.StrictMode>
